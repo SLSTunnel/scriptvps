b3b42ee6e048d844e4e6d1a1641767f4d23bc9b1
SLSTunnel <106473683+SLSTunnel@users.noreply.github.com>
2023-09-22 11:37:18 -0500

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Tarap-Kuhing/perizinan/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SLSTunnel/perizinan/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -27,10 +27,10 @@ echo -e "${NC}${LIGHT}Telegram : https://t.me/Hendra2012"
 exit 0
 fi
 # Link Hosting Kalian
-tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket"
+tarapkuhing="raw.githubusercontent.com/SLSTunnel/scriptvps/main/websocket"
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket/websocket.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/SLSTunnel/scriptvps/main/websocket/websocket.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
@@ -58,7 +58,7 @@ systemctl enable ws-nontls
 systemctl restart ws-nontls
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket/ws-ovpn.py
+wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/SLSTunnel/scriptvps/main/websocket/ws-ovpn.py
 chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
@@ -86,7 +86,7 @@ systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-tls https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket/ws-tls
+wget -q -O /usr/local/bin/ws-tls https://raw.githubusercontent.com/SLSTunnel/scriptvps/main/websocket/ws-tls
 chmod +x /usr/local/bin/ws-tls
 
 # Installing Service
