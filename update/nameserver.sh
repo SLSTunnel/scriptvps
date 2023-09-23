eb518bf6a0d133a2a720e93c096d1022dc67343d
SLSTunnel <106473683+SLSTunnel@users.noreply.github.com>
2023-09-23 03:07:14 -0500

    Update nameserver.sh

--- update/nameserver.sh
+++ update/nameserver.sh
@@ -1,3 +1,17 @@
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/SLSTunnel/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : "
+echo -e "${NC}${LIGHT}WhatsApp : 085754292950"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/Hendra2012"
+exit 0
+fi
 done=$(cat $installed | grep -w "done" | wc -l)
 if [[ ${done} == '1' ]]; then
     echo -ne "Nama Server: "
