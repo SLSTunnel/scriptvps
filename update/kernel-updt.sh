6367d846adf54f24f24fe5cdeda26817f2e2a9fe
SLSTunnel <106473683+SLSTunnel@users.noreply.github.com>
2023-09-23 03:06:44 -0500

    Update kernel-updt.sh

--- update/kernel-updt.sh
+++ update/kernel-updt.sh
@@ -5,7 +5,20 @@ NC='\e[0m'
 clear
 source /etc/os-release
 OS=$ID
-
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/SLSTunnel/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : "
+echo -e "${NC}${LIGHT}WhatsApp : 085754292950"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/Hendra2012"
+exit 0
+fi
 # Memulai Update Kernel
 echo "Start Updating Kernel"
 
